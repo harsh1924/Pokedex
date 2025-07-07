87967d2204bba37bf50561e80dcc84867e3ac639
harsh1924 <harshrpanwar@gmail.com>
2025-07-07 16:12:28 +0530

    Changed Filter UI

--- src/components/Pokedex/Pokedex.jsx
+++ src/components/Pokedex/Pokedex.jsx
@@ -11,6 +11,19 @@ import PseudoLegendaryPokemon from '../PsuedoLegendary/PseudoLegendaryPokemon';
 import ParadoxPokemon from '../ParadoxPokemon/ParadoxPokemon';
 import GigantamaxPokemon from '../GigantamaxPokemon/GigantamaxPokemon';
 
+const buttonViews = [
+    { label: "All", value: "default" },
+    { label: "Starter", value: "starter" },
+    { label: "Strongest", value: "strongest" },
+    { label: "Mythical", value: "mythical" },
+    { label: "Legendary", value: "legendary" },
+    { label: "Ultra Beasts", value: "special" },
+    { label: "Mega Evolution", value: "mega" },
+    { label: "Pseudo-Legendary", value: "pseudo" },
+    { label: "Paradox", value: "paradox" },
+    { label: "Gigantamax", value: "gmax" },
+];
+
 const Pokedex = () => {
     const [search, setSearch] = useState("");
     const [view, setView] = useState("default");
@@ -42,71 +55,27 @@ const Pokedex = () => {
 
             <Search setSearch={setSearch} />
 
-            <div className="overflow-x-auto w-full scrollbar-hide px-4">
+            <div className="overflow-x-auto w-full scrollbar-hide px-4 h-[60px] flex items-center font-serif">
                 <div className="flex whitespace-nowrap gap-3">
-                    <button
-                        onClick={() => handleViewChange("default")}
-                        className='rounded-md bg-rose-500 hover:bg-rose-600 px-4 py-2 text-white font-bold tracking-wide text-lg transition-transform hover:scale-105 shadow cursor-pointer min-w-fit'
-                    >
-                        All
-                    </button>
-                    <button
-                        onClick={() => handleViewChange("starter")}
-                        className="rounded-md bg-emerald-500 hover:bg-emerald-600 px-4 py-2 text-white font-bold tracking-wide text-lg transition-transform hover:scale-105 shadow cursor-pointer min-w-fit"
-                    >
-                        Starter
-                    </button>
-
-                    <button
-                        onClick={() => handleViewChange("strongest")}
-                        className="rounded-md bg-red-500 hover:bg-red-600 px-4 py-2 text-white font-bold tracking-wide text-lg transition-transform hover:scale-105 shadow cursor-pointer min-w-fit"
-                    >
-                        Strongest
-                    </button>
-                    <button
-                        onClick={() => handleViewChange("mythical")}
-                        className="rounded-md bg-purple-500 hover:bg-purple-600 px-4 py-2 text-white font-bold tracking-wide text-lg transition-transform hover:scale-105 shadow cursor-pointer min-w-fit"
-                    >
-                        Mythical
-                    </button>
-                    <button
-                        onClick={() => handleViewChange("legendary")}
-                        className="rounded-md bg-yellow-500 hover:bg-yellow-600 px-4 py-2 text-white font-bold tracking-wide text-lg transition-transform hover:scale-105 shadow cursor-pointer min-w-fit"
-                    >
-                        Legendary
-                    </button>
-                    <button
-                        onClick={() => handleViewChange("special")}
-                        className="rounded-md bg-pink-500 hover:bg-pink-600 px-4 py-2 text-white font-bold tracking-wide text-lg transition-transform hover:scale-105 shadow cursor-pointer min-w-fit"
-                    >
-                        Ultra Beasts
-                    </button>
-                    <button
-                        onClick={() => handleViewChange("mega")}
-                        className="rounded-md bg-indigo-500 hover:bg-indigo-600 px-4 py-2 text-white font-bold tracking-wide text-lg transition-transform hover:scale-105 shadow cursor-pointer min-w-fit"
-                    >
-                        Mega Evolution
-                    </button>
-                    <button
-                        onClick={() => handleViewChange("pseudo")}
-                        className="rounded-md bg-[#0d9488] hover:bg-[#0f766e] px-4 py-2 text-white font-bold tracking-wide text-lg transition-transform hover:scale-105 min-w-fit shadow cursor-pointer"
-                    >
-                        Pseudo-Legendary
-                    </button>
-                    <button
-                        onClick={() => handleViewChange("paradox")}
-                        className="rounded-md bg-[#ec4899] hover:bg-[#db2777] px-4 py-2 text-white font-bold tracking-wide text-lg transition-transform hover:scale-105 min-w-fit shadow cursor-pointer"
-                    >
-                        Paradox
-                    </button>
-                    <button
-                        onClick={() => handleViewChange("gmax")}
-                        className="rounded-md bg-[#f59e0b] hover:bg-[#d97706] px-4 py-2 text-white font-bold tracking-wide text-lg transition-transform hover:scale-105 min-w-fit shadow cursor-pointer"
-                    >
-                        Gigantamax
-                    </button>
+                    {buttonViews.map(({ label, value }) => {
+                        const isActive = view === value;
+                        return (
+                            <button
+                                key={value}
+                                onClick={() => handleViewChange(value)}
+                                className={`min-w-fit px-4 py-2 rounded-md font-extrabold tracking-wide text-lg transition-transform hover:scale-105 shadow-[4px_4px_0_0_#000] translate-y-[-4px] border-2 cursor-pointer
+                                        ${isActive
+                                        ? "bg-red-500 text-white border-black"
+                                        : "bg-white text-black border-black"
+                                    }`}
+                            >
+                                {label}
+                            </button>
+                        );
+                    })}
                 </div>
             </div>
+
 
             <div className="h-[1px] bg-gray-400 mt-3" />
 
